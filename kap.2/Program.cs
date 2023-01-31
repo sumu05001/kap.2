31583bee6d18d459bd62f8909206e5414415a488
Tsnem Musa <tm222qf@student.lnu.se>
2023-01-31 15:23:04 +0100

    Add project files.

---
+++ kap.2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace uppgift2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt Elin hoppade");
+            double elinhappade = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Hur långt Alma hoppade");
+            double almahappade = Convert.ToDouble(Console.ReadLine());
+            double skilland = elinhappade - almahappade;
+            Console.WriteLine("Elin hoppade " + skilland + " meter mer än Alma");
+        }
+    }
+}
+      
+
+
+
+
